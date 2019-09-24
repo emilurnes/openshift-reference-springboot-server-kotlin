7c6cdf5eec456e85d3d572ac0221b0a0f5fc4e01
Emil Urnes <Emil.Urnes@skatteetaten.no>
2019-09-24 14:54:33 +0200

    test av applikasjonskonfig

---
+++ src/test/kotlin/no/skatteetaten/aurora/openshift/reference/springboot/ApplicationTest.kt
@@ -0,0 +1,14 @@
+package no.skatteetaten.aurora.openshift.reference.springboot
+
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    @Throws(Exception::class)
+    fun contextLoads() {
+    }
+}
